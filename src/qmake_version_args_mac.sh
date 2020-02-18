9b2a4d42924cc3842391143608af573c3e8e9cda
Adam <adam@mahletconsulting.com>
2020-02-18 17:54:55 -0500

    ci: try another fix for mac escape strings

--- src/qmake_version_args_mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo DEFINES+=\"HOBBITS_GUI_VERSION=\\\"\\\\\\\\\"`../print_version.sh`\\\\\\\\\"\\\"\" DEFINES+=\"HOBBITS_CORE_LIB_VERSION=\\\"\\\\\\\\\"`../print_version.sh`\\\\\\\\\"\\\"\" DEFINES+=\"HOBBITS_RUNNER_VERSION=\\\"\\\\\\\\\"`../print_version.sh`\\\\\\\\\"\\\"\"
