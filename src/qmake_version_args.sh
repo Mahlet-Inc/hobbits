cacea5d097ff1d41c74cf30435392e5f371a3a64
Adam <adam@mahletconsulting.com>
2020-02-18 18:01:22 -0500

    ci: try to fix mac and ubuntu version args

--- src/qmake_version_args.sh
+++ src/qmake_version_args.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo DEFINES+=\"HOBBITS_GUI_VERSION=\\\"\\\\\\\"`../print_version.sh`\\\\\\\"\\\"\" DEFINES+=\"HOBBITS_CORE_LIB_VERSION=\\\"\\\\\\\"`../print_version.sh`\\\\\\\"\\\"\" DEFINES+=\"HOBBITS_RUNNER_VERSION=\\\"\\\\\\\"`../print_version.sh`\\\\\\\"\\\"\"
+echo DEFINES+=\"HOBBITS_GUI_VERSION=\\\"\\\\\\\"`node -pe "require('../package.json').version"`\\\\\\\"\\\"\" DEFINES+=\"HOBBITS_CORE_LIB_VERSION=\\\"\\\\\\\"`node -pe "require('../package.json').version"`\\\\\\\"\\\"\" DEFINES+=\"HOBBITS_RUNNER_VERSION=\\\"\\\\\\\"`node -pe "require('../package.json').version"`\\\\\\\"\\\"\"
