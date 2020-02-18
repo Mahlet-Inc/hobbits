af7629491aa743c23bb3ac5c996c1c1a3c03e0cd
Adam <adam@mahletconsulting.com>
2020-02-18 16:25:31 -0500

    ci: fix core lib define in qmake args

--- src/qmake_version_args.sh
+++ src/qmake_version_args.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo DEFINES+=HOBBITS_GUI_VERSION=\"\\\"`../print_version.sh`\\\"\" DEFINES+=HOBBITS_CORE_VERSION=\"\\\"`../print_version.sh`\\\"\" DEFINES+=HOBBITS_RUNNER_VERSION=\"\\\"`../print_version.sh`\\\"\"
+echo DEFINES+=HOBBITS_GUI_VERSION=\"\\\"`../print_version.sh`\\\"\" DEFINES+=HOBBITS_CORE_LIB_VERSION=\"\\\"`../print_version.sh`\\\"\" DEFINES+=HOBBITS_RUNNER_VERSION=\"\\\"`../print_version.sh`\\\"\"
