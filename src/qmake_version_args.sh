2878e52e00b2fcdbc7819a2bc066d03c936b3fca
Adam <adam@mahletconsulting.com>
2020-02-19 21:09:32 -0500

    fix(release): fix formatting of windows version number in build

--- src/qmake_version_args.sh
+++ src/qmake_version_args.sh
@@ -6,4 +6,4 @@ then
   export HOBBITS_VERSION=`cat ../.version`
 fi
 
-echo DEFINES+=\"HOBBITS_GUI_VERSION=\\\"\\\\\\\"$HOBBITS_VERSION\\\\\\\"\\\"\" DEFINES+=\"HOBBITS_CORE_LIB_VERSION=\\\"\\\\\\\"$HOBBITS_VERSION\\\\\\\"\\\"\" DEFINES+=\"HOBBITS_RUNNER_VERSION=\\\"\\\\\\\"$HOBBITS_VERSION\\\\\\\"\\\"\"
+echo DEFINES+=\"HOBBITS_GUI_VERSION=\\\"\\\\\\\"`echo $HOBBITS_VERSION`\\\\\\\"\\\"\" DEFINES+=\"HOBBITS_CORE_LIB_VERSION=\\\"\\\\\\\"`echo $HOBBITS_VERSION`\\\\\\\"\\\"\" DEFINES+=\"HOBBITS_RUNNER_VERSION=\\\"\\\\\\\"`echo $HOBBITS_VERSION`\\\\\\\"\\\"\"
