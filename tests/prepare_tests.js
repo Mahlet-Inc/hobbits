bb9277ff133e900976b851eb7417a9777f6ea316
Adam <adam@mahletconsulting.com>
2020-03-04 12:58:23 -0500

    chore: remove git lfs now that tests are just compressed

--- tests/prepare_tests.js
+++ tests/prepare_tests.js
@@ -3,14 +3,22 @@
 const sevenBin = require('7zip-bin')
 const { extractFull } = require('node-7z')
 const del = require('del');
+const { existsSync } = require('fs')
 
 // Delete current extracted folders
 let extractedTests = './extracted_tests'
 let testArchive = './test-archive.7z'
-del.sync(extractedTests)
+if (existsSync(extractedTests)) {
+    console.log(`Deleting existing ${extractedTests} folder...`)
+    del.sync(extractedTests)
+}
 
 // Make new test archive
 const pathTo7zip = sevenBin.path7za
-const seven = extractFull(testArchive, '.', {
+const archiveStream = extractFull(testArchive, '.', {
   $bin: pathTo7zip
+})
+
+archiveStream.on('end', () => {
+    console.log(`Tests extracted into ${extractedTests}. Run them with test_hobbits.js`)
 })
--- tests/repackage_tests.js
+++ tests/repackage_tests.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
 const sevenBin = require('7zip-bin')
-const { extractFull, add } = require('node-7z')
+const { add } = require('node-7z')
 
-const { readdirSync, unlinkSync, existsSync } = require('fs')
-const { join } = require('path')
-const { execFileSync } = require("child_process");
+const { unlinkSync, existsSync } = require('fs')
 const glob = require('glob');
 
 // Delete test output
+console.log(`Removing test output before archiving...`)
 let testOutputMatches = glob.sync('extracted_tests/**/testrunoutput.*', {nonull: false})
 for (let oldOutput of testOutputMatches) {
     unlinkSync(oldOutput)
@@ -17,13 +16,19 @@ for (let oldOutput of testOutputMatches) {
 // Delete old test archive
 let testArchive = './test-archive.7z'
 if (existsSync(testArchive)) {
+    console.log(`Removing old ${testArchive}...`)
     unlinkSync(testArchive)
 }
 
 // Make new test archive
+console.log(`Creating new archive...`)
 const pathTo7zip = sevenBin.path7za
-
-const myStream = add(testArchive, './extracted_tests', {
+const archiveStream = add(testArchive, './extracted_tests', {
+    latestTimestamp: true,
     recursive: true,
     $bin: pathTo7zip
+})
+
+archiveStream.on('end', () => {
+    console.log(`Tests packaged into ${testArchive} and ready to commit`)
 })
