dd9928545062ea6735b41845d6d2c80f74c294b3
Adam <adam@mahletconsulting.com>
2020-04-29 16:21:48 -0400

    chore: remove stray FrequencyPlot project skeleton

--- src/hobbits-plugins/FrequencyPlot/frequencyplotcontrols.cpp
+++
@@ -1,9 +0,0 @@
-#include "frequencyplotcontrols.h"
-#include "ui_frequencyplotcontrols.h"
-
-
-FrequencyPlotControls::FrequencyPlotControls() :
-    ui(new Ui::FrequencyPlotControls())
-{
-    ui->setupUi(this);
-}
