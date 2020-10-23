81fb8a72c2a98c5a4360b3e2149620ba43ee10b3
Adam <adam@mahletconsulting.com>
2020-10-23 18:03:17 -0400

    fix: handle larger word sizes correctly in Frequency Plot

--- src/hobbits-plugins/displays/FrequencyPlot/frequencyplotwidget.cpp
+++ src/hobbits-plugins/displays/FrequencyPlot/frequencyplotwidget.cpp
@@ -29,33 +29,61 @@ void FrequencyPlotWidget::paintEvent(QPaintEvent*) {
     }
     qint64 startBit = (frames->at(frameOffset).start() / m_wordSize) * m_wordSize;
 
-    int plotSize = 1 << m_wordSize;
+    quint64 barCount = 1ull << m_wordSize;
+    int valueIdxShift = 0;
+    if (m_wordSize == 64) {
+        barCount = 2048;
+        valueIdxShift = 53;
+    }
     QRgb background = qRgb(0, 0, 0);
     QRgb foreground = qRgb(200, 230, 255);
 
-    QVector<int> wordFrequency(plotSize);
-    int frequencyMax = 1;
+    while (barCount > 2048) {
+        barCount >>= 1;
+        valueIdxShift++;
+    }
+
+    int plotSize = int(barCount);
+
+    QMap<quint64, int> frequencies;
     for (qint64 word = 0; word < m_windowSize; word++) {
         qint64 start = startBit + word * m_wordSize;
         qint64 end = start + m_wordSize;
         if (bits->sizeInBits() < end) {
             break;
         }
-        int wordValue = int(bits->parseUIntValue(start, int(m_wordSize)));
-        wordFrequency[wordValue]++;
-        frequencyMax = qMax(frequencyMax, wordFrequency.at(wordValue));
+        quint64 wordValue = bits->parseUIntValue(start, int(m_wordSize));
+        if (!frequencies.contains(wordValue)) {
+            frequencies.insert(wordValue, 0);
+        }
+        frequencies[wordValue]++;
     }
+
+    QMap<int, quint64> barMax;
+    QVector<int> barFrequencies(plotSize);
+    int frequencyMax = 1;
+    for (quint64 wordValue : frequencies.keys()) {
+        quint64 idx = wordValue >> valueIdxShift;
+        int frequency = frequencies.value(wordValue);
+        if (frequency > barFrequencies.at(int(idx))) {
+            barFrequencies[int(idx)] = frequencies.value(wordValue);
+            barMax.insert(int(idx), wordValue);
+            frequencyMax = qMax(frequencyMax, frequency);
+        }
+    }
+
 
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing, false);
     painter.scale(m_scale, m_scale);
 
-    painter.fillRect(0, 0, plotSize, plotSize, background);
+    int plotHeight = qMin(plotSize, 512);
+    painter.fillRect(0, 0, plotSize, plotHeight, background);
 
-    double ratio = -1.0*double(plotSize)/double(frequencyMax);
-    for (int x = 0; x < wordFrequency.size(); x++) {
-        double amount = double(wordFrequency.at(x))*ratio;
-        painter.fillRect(QRectF(x, plotSize, 1, amount), foreground);
+    double ratio = -1.0*double(plotHeight)/double(frequencyMax);
+    for (int x = 0; x < barFrequencies.size(); x++) {
+        double amount = double(barFrequencies.at(x))*ratio;
+        painter.fillRect(QRectF(x, plotHeight, 1, amount), foreground);
     }
 }
 
