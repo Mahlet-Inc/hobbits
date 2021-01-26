171554c9bc601701767018108c1912d96e701cce
Adam <adam@mahletconsulting.com>
2021-01-26 18:44:59 -0500

    build: configure mac bundle build

--- cmake/PackExternalDeps.cmake
+++ cmake/PackExternalDeps.cmake
@@ -3,45 +3,45 @@ function(pack_qt_libs)
 
     get_target_property(QT_CORE_LIB Qt5::Core LOCATION)
     get_filename_component(QT_LIB_DIR "${QT_CORE_LIB}" DIRECTORY)
-
-    file(GLOB LIBICU "${QT_LIB_DIR}/libicu*.so*")
-    file(GLOB LIBCORE "${QT_LIB_DIR}/libQt5Core.so*")
-    file(GLOB LIBCON "${QT_LIB_DIR}/libQt5Concurrent.so*")
-    file(GLOB LIBDBUS "${QT_LIB_DIR}/libQt5DBus.so*")
-    file(GLOB LIBGUI "${QT_LIB_DIR}/libQt5Gui.so*")
-    file(GLOB LIBNET "${QT_LIB_DIR}/libQt5Network.so*")
-    file(GLOB LIBPRNT "${QT_LIB_DIR}/libQt5PrintSupport.so*")
-    file(GLOB LIBWAY "${QT_LIB_DIR}/libQt5WaylandClient.so*")
-    file(GLOB LIBWID "${QT_LIB_DIR}/libQt5Widgets.so*")
-    file(GLOB LIBXCB "${QT_LIB_DIR}/libQt5XcbQpa.so*")
-
-    list(APPEND QTLIBS
-                ${LIBICU}
-                ${LIBCORE}
-                ${LIBCON}
-                ${LIBDBUS}
-                ${LIBGUI}
-                ${LIBNET}
-                ${LIBPRNT}
-                ${LIBWID}
-                ${LIBWAY}
-                ${LIBXCB}
-                )
-    
-    list(FILTER QTLIBS EXCLUDE REGEX .*debug$)
-
     get_filename_component(QT_ROOT_DIR "${QT_LIB_DIR}" DIRECTORY)
     set(QT_PLATFORM_DIR "${QT_ROOT_DIR}/plugins/platforms")
-    file(GLOB QTPLATFORMS "${QT_PLATFORM_DIR}/*.so*")
-    list(FILTER QTPLATFORMS EXCLUDE REGEX .*debug$)
 
-    install(FILES ${QTLIBS}
-            DESTINATION "${CMAKE_INSTALL_LIBDIR}")
+    if (LINUX)
+        file(GLOB LIBICU "${QT_LIB_DIR}/libicu*.so*")
+        file(GLOB LIBCORE "${QT_LIB_DIR}/libQt5Core.so*")
+        file(GLOB LIBCON "${QT_LIB_DIR}/libQt5Concurrent.so*")
+        file(GLOB LIBDBUS "${QT_LIB_DIR}/libQt5DBus.so*")
+        file(GLOB LIBGUI "${QT_LIB_DIR}/libQt5Gui.so*")
+        file(GLOB LIBNET "${QT_LIB_DIR}/libQt5Network.so*")
+        file(GLOB LIBPRNT "${QT_LIB_DIR}/libQt5PrintSupport.so*")
+        file(GLOB LIBWAY "${QT_LIB_DIR}/libQt5WaylandClient.so*")
+        file(GLOB LIBWID "${QT_LIB_DIR}/libQt5Widgets.so*")
+        file(GLOB LIBXCB "${QT_LIB_DIR}/libQt5XcbQpa.so*")
 
-    install(FILES ${QTPLATFORMS}
-            DESTINATION "${CMAKE_INSTALL_BINDIR}/platforms")
+        list(APPEND QTLIBS
+                    ${LIBICU}
+                    ${LIBCORE}
+                    ${LIBCON}
+                    ${LIBDBUS}
+                    ${LIBGUI}
+                    ${LIBNET}
+                    ${LIBPRNT}
+                    ${LIBWID}
+                    ${LIBWAY}
+                    ${LIBXCB}
+                    )
 
-    if (UNIX AND NOT APPLE)
+        list(FILTER QTLIBS EXCLUDE REGEX .*debug$)
+
+        file(GLOB QTPLATFORMS "${QT_PLATFORM_DIR}/*.so*")
+        list(FILTER QTPLATFORMS EXCLUDE REGEX .*debug$)
+
+        install(FILES ${QTLIBS}
+                DESTINATION "${CMAKE_INSTALL_LIBDIR}")
+
+        install(FILES ${QTPLATFORMS}
+                DESTINATION "${CMAKE_INSTALL_BINDIR}/platforms")
+
         foreach(PLATFILEFULL ${QTPLATFORMS})
             get_filename_component(PLATFILE "${PLATFILEFULL}" NAME)
             install(
@@ -56,6 +56,15 @@ function(pack_qt_libs)
                 )"
             )
         endforeach()
+    elseif(APPLE)
+        file(GLOB QTPLATFORMS "${QT_PLATFORM_DIR}/*.dylib")
+        install(FILES ${QTPLATFORMS}
+                DESTINATION "hobbits.app/Contents/PlugIns/platforms")
+        install(DIRECTORY "${QT_LIB_DIR}/QtPrintSupport.framework"
+                          "${QT_LIB_DIR}/QtDBus.framework"
+                          "${QT_LIB_DIR}/QtNetwork.framework"
+                          "${QT_LIB_DIR}/QtConcurrent.framework"
+                DESTINATION "hobbits.app/Contents/Frameworks")
     endif()
 
 endfunction(pack_qt_libs)
@@ -64,34 +73,56 @@ function(pack_python)
     get_target_property(PYBIN Python3::Interpreter LOCATION)
     get_filename_component(PYBIN_DIR "${PYBIN}" DIRECTORY)
     get_filename_component(PYROOT_DIR "${PYBIN_DIR}" DIRECTORY)
-    
+
+    set(PY_DEST_DIR "python")
+    set(LIB_WILDCARD "*.so")
+    if (APPLE)
+        set(PY_DEST_DIR "hobbits.app/Contents/Frameworks/python")
+        set(LIB_WILDCARD "*.dylib")
+    elseif(WIN32)
+        set(LIB_WILDCARD "*.dll")
+    endif()
+
     install( DIRECTORY "${PYROOT_DIR}/bin"
-                DESTINATION "python"
+                DESTINATION "${PY_DEST_DIR}"
                 COMPONENT runtime
                 PATTERN "*"
                 PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE
                             GROUP_READ GROUP_EXECUTE
                             WORLD_READ WORLD_EXECUTE)
     install( DIRECTORY "${PYROOT_DIR}/lib"
-                DESTINATION "python"
+                DESTINATION "${PY_DEST_DIR}"
                 COMPONENT runtime
-                PATTERN "*.so"
+                PATTERN "${LIB_WILDCARD}"
                 PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE
                             GROUP_READ GROUP_EXECUTE
                             WORLD_READ WORLD_EXECUTE)
 endfunction(pack_python)
 
 function(pack_fftw)
-    get_target_property(FFTWLIB FFTW::Double INTERFACE_LINK_LIBRARIES)
-    string(REGEX REPLACE \.so\..*$ ".so" BASELIB ${FFTWLIB})
-    file(GLOB FFTWLIBS "${BASELIB}*")
-    install(FILES ${FFTWLIBS}
-            DESTINATION "${CMAKE_INSTALL_LIBDIR}")
+    if (LINUX)
+        get_target_property(FFTWLIB FFTW::Double INTERFACE_LINK_LIBRARIES)
+        string(REGEX REPLACE \.so\..*$ ".so" BASELIB ${FFTWLIB})
+        file(GLOB FFTWLIBS "${BASELIB}*")
+        install(FILES ${FFTWLIBS}
+                DESTINATION "${CMAKE_INSTALL_LIBDIR}")
+    elseif(APPLE)
+        get_target_property(FFTWLIB FFTW::Double INTERFACE_LINK_LIBRARIES)
+        install(FILES ${FFTWLIBS}
+                DESTINATION "hobbits.app/Contents/Frameworks")
+    elseif(WIN32)
+    endif()
 endfunction(pack_fftw)
 
 function(pack_pcap)
-    string(REGEX REPLACE \.so\..*$ ".so" BASELIB ${PCAP_LIBRARY})
-    file(GLOB PCAPLIBS "${BASELIB}*")
-    install(FILES ${PCAPLIBS}
-            DESTINATION "${CMAKE_INSTALL_LIBDIR}")
+    if (LINUX)
+        string(REGEX REPLACE \.so\..*$ ".so" BASELIB ${PCAP_LIBRARY})
+        file(GLOB PCAPLIBS "${BASELIB}*")
+        install(FILES ${PCAPLIBS}
+                DESTINATION "${CMAKE_INSTALL_LIBDIR}")
+    elseif(APPLE)
+        install(FILES ${PCAP_LIBRARY}
+                DESTINATION "hobbits.app/Contents/Frameworks")
+    elseif(WIN32)
+    endif()
 endfunction(pack_pcap)
