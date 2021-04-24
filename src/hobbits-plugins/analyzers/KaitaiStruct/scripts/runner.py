03067d421d7f221791bb92377c4356f23c2566d1
Adam <adam@mahletconsulting.com>
2021-04-24 12:37:02 -0400

    fix: appropriately offset imported sub-sections in kaitai (#93)

--- src/hobbits-plugins/analyzers/KaitaiStruct/scripts/runner.py
+++ src/hobbits-plugins/analyzers/KaitaiStruct/scripts/runner.py
@@ -10,7 +10,7 @@ from kaitaistruct import KaitaiStruct
 #import pprint
 #pp = pprint.PrettyPrinter(indent=2)
 
-def dump_struct(s, sections, prefix=""):
+def dump_struct(s, sections, prefix="", parent_offset = 0, root_io=None, root_offset=0):
     if isinstance(s, list):
         #print(f"\n[BEGIN List Under '{prefix}']")
         for i, item in enumerate(s):
@@ -20,27 +20,36 @@ def dump_struct(s, sections, prefix=""):
                 "label": label,
                 "parent": prefix
             })
-            dump_struct(item, sections, label)
+            dump_struct(item, sections, label, parent_offset, root_io, root_offset)
         #print(f"[END OF List Under '{prefix}']\n")
     elif isinstance(s, KaitaiStruct):
         #pp.pprint(vars(s))
         if hasattr(s, "_debug"):
 
+            section_io = s._io
+            if root_io is None:
+                root_io = section_io
+            elif section_io is not root_io:
+                root_io = section_io
+                root_offset = root_offset + parent_offset
+
             #print("\nITEMS {\n")
             for name, descr in s._debug.items():
                 prop = getattr(s, name)
                 label = prefix + "." + name if prefix else name
-                #print(f"(\n  name: {label},\n  descr: {descr},\n  prop: {prop},\n  offset: {offset}\n)\n")
-                #if isinstance(prop, KaitaiStruct):
-                #    print(vars(prop))
+                #print(f"(\n  name: {label},\n  descr: {descr},\n  prop: {prop},\n  offset: {root_offset}\n)\n")
+
                 sections.append({
-                    "start": descr["start"],
-                    "end": descr["end"],
+                    "start": descr["start"] + root_offset,
+                    "end": descr["end"] + root_offset,
                     "label": label,
                     "parent": prefix
                 })
 
-                dump_struct(prop, sections, label)
+
+                parent_offset = descr["start"] + root_offset
+
+                dump_struct(prop, sections, label, parent_offset, root_io, root_offset)
 
             #print("}\n")
 
@@ -66,6 +75,7 @@ def parse_data(input_filename, output_filename, action_progress):
 
     # write the parser result to the output
     sections = []
+
     dump_struct(target, sections)
 
     #pp.pprint(sections)
