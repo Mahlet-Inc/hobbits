a11154bdf1296112b614c4f759684cb5f4116094
melissascode <melissafranz9211@gmail.com>
2021-10-07 11:12:29 -0400

    Trying to resolve merge conflict w/ conanfile.py and making changes from pull request.

--- conanfile.py
+++ conanfile.py
@@ -34,7 +34,7 @@ class HobbitsConan(ConanFile):
 
     requires = [
         ("qt/5.15.2"),
-        ("hobbits-cpython/3.9.1"),
+        ("hobbits-cpython/3.9.7"),
         ("pffft/cci.20210511"),
         ("libusb/1.0.24")
     ]
